b48a59a12708e1a2d7f93ca0a6f84d94c2cc0c5e
Ted Pattison <ted@tedpattison.net>
2020-11-13 17:06:31 -0500

    Updates

---
+++ Solution/Exercise01/PBI-Tool/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AnalysisServices.Tabular;
+ 
+class Program {
+
+    const string connectString = "localhost:50468"; // update for port number on your machine
+
+    static void Main(string[] args) {
+        
+        Server server = new Server();
+        server.Connect(connectString);
+        
+        Model model = server.Databases[0].Model;
+        
+        // foreach(Table table in model.Tables) {
+        //     Console.WriteLine($"Table : {table.Name}");
+        // }
+
+        Table table = model.Tables["Sales"];
+
+        if (table.Measures.ContainsName("VS Code Measure")) {
+            Measure measure = table.Measures["VS Code Measure"];
+            measure.Expression = "\"Hello Again World\"";
+        }
+        else {
+            Measure measure = new Measure() {
+                Name = "VS Code Measure",
+                Expression = "\"Hello World\""
+            };
+            table.Measures.Add(measure);
+        }
+
+        model.SaveChanges();
+
+    }
+}
+
+
